1a744867d9d80d0d85aac6cf5f8f0895c5f548ce
Swati Makhija <swatimakhija@Swatis-MacBook-Air.local>
2021-03-23 10:55:36 -0700

    model migrations, added feedings, and added display

--- main_app/models.py
+++ main_app/models.py
@@ -12,3 +12,26 @@ class Cat(models.Model):
 
 # garfield = Cat('Garfield', 'Tabby', 'I have never heard of Tabby', 9)
 # print(garfield)
+
+# tuples (value1, value2, value3) immutable. pop() doesn't work
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner')
+)
+
+class Feeding(models.Model):
+    date = models.DateField()
+    meal = models.CharField(
+        max_length = 1,
+        #possible choices are the MEALS
+        choices = MEALS,
+        default = MEALS[0][0]
+    )
+
+    #what happens if we delete the parent node, in this case a specific cat
+    #association to Cat Model, when cat is deleted, delete associated feedings
+    cat = models.ForeignKey(Cat, on_delete=models.CASCADE )
+    
+    def __str__(self):
+        return f"{self.get_meal_display()} on {self.date}"
